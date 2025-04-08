d98f480dc0bdcc60f2a3d633bc5fb88ec412acfc
Muskan Sudiksha <muskansudiksha2000@gmail.com>
2025-04-08 16:34:50 +0530

    First milestone two

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,20 +1,49 @@
-
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar/Navbar.jsx';
-import HeroSection from "./components/Hero/Hero.jsx";
-// import AlbumSection from "./components/AlbumSection/AlbumSection.jsx";
-// import FaqSection from './components/FaqSection/FaqSection.jsx';
+import React, { useEffect, useState } from "react";
+import Hero from "./components/Hero/Hero";
+import HomePage from "./pages/HomePage/HomePage";
+import Navbar from "./components/Navbar/Navbar";
+import StyledEngineProvider from "@mui/material/StyledEngineProvider";
+import { Outlet } from "react-router-dom";
+import {
+  fetchFilters,
+  fetchNewAlbums,
+  fetchSongs,
+  fetchTopAlbums,
+} from "./api/api";
 
 function App() {
+  const [data, setData] = useState({});
+
+  
+
+  const generateData = (key, source) => {
+    source().then((data) => {
+      setData((prevState) => {
+        // Object.assign would also work
+        return { ...prevState, [key]: data };
+      });
+    });
+  };
+
+  useEffect(() => {
+    generateData("topAlbums", fetchTopAlbums);
+    generateData("newAlbums", fetchNewAlbums);
+    generateData("songs", fetchSongs);
+    generateData("genres", fetchFilters);
+  }, []);
+
+  const { topAlbums = [], newAlbums = [], songs = [], genres = [] } = data;
+
   return (
     <>
-      <Navbar />
-      <HeroSection />
-      {/* <AlbumSection />
-      <FaqSection /> */}
+      <StyledEngineProvider injectFirst>
+        <Navbar searchData={[...topAlbums, ...newAlbums]} />
+        <Outlet context={{ data: { topAlbums, newAlbums, songs, genres } }} />
+      </StyledEngineProvider>
     </>
   );
 }
+
+
 
 export default App;
--- qtify/src/index.js
+++ qtify/src/index.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import HomePage from "./pages/HomePage/HomePage";
+import "./index.css";
+import AlbumPage from "./pages/AlbumPage/AlbumPage";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/album/:albumId",
+        element: <AlbumPage />,
+      },
+    ],
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<RouterProvider router={router} />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
